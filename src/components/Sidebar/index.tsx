b562c1d77194eab736983cdf9b9b31c7977720c8
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-06 12:41:15 +0500

    add new items

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Logo, Leaves } from "../../assets";
 import { CLIENT_PAGES } from "../../routes";
 
 interface ILinkItem {
@@ -21,14 +22,11 @@ const LinkComponent = ({ item }: ILinkItem) => {
 
 const index = () => {
   return (
-    <div className="sidebar-bg h-full w-full">
-      <div className="flex items-center justify-normal md-lg:flex-col md-lg:justify-between">
-        <div className="md-lg:mt-10">
+    <div className="sidebar-bg h-full w-full overflow-hidden">
+      <div className="h-full flex items-center justify-normal md-lg:flex-col md-lg:justify-between">
+        <div className="md-lg:mt-10 md-lg:max-w-full">
           <Link to={"/"}>
-            <img
-              src="https://max-themes.net//demos/albertos/images/logo.png"
-              alt="company logo"
-            />
+            <img src={Logo} alt="company logo" className="w-full" />
           </Link>
         </div>
 
@@ -37,6 +35,17 @@ const index = () => {
             <LinkComponent key={item.id} item={item} />
           ))}
         </ul>
+
+        <div className="max-w-full mt-auto -mb-10 hidden md-lg:block">
+          <img
+            src={Leaves}
+            alt="leaves"
+            style={{
+              transform: "rotate(-10deg)",
+            }}
+            className="w-2/3 mx-auto"
+          />
+        </div>
       </div>
     </div>
   );
