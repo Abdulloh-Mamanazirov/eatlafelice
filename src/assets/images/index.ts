1435e8e648dfefba39f687a4355e540acc4b5cdb
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-06 12:41:27 +0500

    add new images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+import Logo from "./logo.png";
+import Leaves from "./leaves.png";
+
+export * from "./pizza";
+export { Logo, Leaves };
---
+++ src/assets/images/pizza/index.ts
@@ -0,0 +1,10 @@
+import Pizza1 from "./1.jpg";
+import Pizza2 from "./2.jpg";
+import Pizza3 from "./3.jpg";
+import Pizza4 from "./4.jpg";
+import Pizza5 from "./5.jpg";
+import Pizza6 from "./6.webp";
+import Pizza7 from "./7.webp";
+import Pizza8 from "./8.webp";
+
+export { Pizza1, Pizza2, Pizza3, Pizza4, Pizza5, Pizza6, Pizza7, Pizza8 };
