469d26b0300bbca78a67a2a5ec7b194b9b19e01c
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-08 16:15:30 +0500

    add navbar

--- src/layout/Client.tsx
+++ src/layout/Client.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import { CLIENT_PAGES } from "../routes";
-import { Sidebar } from "../components";
+import { Navbar, Sidebar } from "../components";
 
 const CLIENT_ROUTER = () => {
   return (
     <div className="md-lg:h-screen md-lg:max-h-screen md-lg:min-h-screen md-lg:overflow-y-auto md-lg:grid grid-cols-6">
-      <div className="md-lg:col-span-1">
+      <div className="z-[999] block sticky top-0 md-lg:hidden">
+        <Navbar />
+      </div>
+      <div className="hidden md-lg:block md-lg:col-span-1">
         <Sidebar />
       </div>
-      <div className="md-lg:col-span-5 max-h-screen overflow-y-auto">
+      <div className="md-lg:col-span-5 md-lg:max-h-screen overflow-y-auto overflow-x-hidden">
         <Routes>
           {CLIENT_PAGES.filter((page) => page.show).map((page) => (
             <Route key={page.id} path={page.path} element={page.element} />
