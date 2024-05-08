75949ce1cc93ae2610e69fb11531bcd4763a5bec
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-08 16:14:31 +0500

    move interface; minor changes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,11 @@
+import type { ILinkItem } from "../../interfaces";
 import { Link } from "react-router-dom";
 import { Logo, Leaves } from "../../assets";
 import { CLIENT_PAGES } from "../../routes";
 
-interface ILinkItem {
-  item: {
-    id: number;
-    path: string;
-    title: string;
-    element: JSX.Element;
-    show: boolean;
-  };
-}
-
 const LinkComponent = ({ item }: ILinkItem) => {
   return (
-    <div className="transition hover:text-primary">
+    <div className="transition hover:text-primary mb-2">
       <Link to={item.path}>{item.title}</Link>
     </div>
   );
@@ -23,20 +14,20 @@ const LinkComponent = ({ item }: ILinkItem) => {
 const index = () => {
   return (
     <div className="sidebar-bg h-full w-full overflow-hidden">
-      <div className="h-full flex items-center justify-normal md-lg:flex-col md-lg:justify-between">
-        <div className="md-lg:mt-10 md-lg:max-w-full">
+      <div className="h-full flex flex-col justify-between">
+        <div className="mt-10 max-w-full">
           <Link to={"/"}>
             <img src={Logo} alt="company logo" className="w-full" />
           </Link>
         </div>
 
-        <ul className="hidden md-lg:block font-patua text-white mt-20 text-2xl text-center">
+        <ul className="font-patua text-white mt-20 text-2xl text-center">
           {CLIENT_PAGES.map((item) => (
             <LinkComponent key={item.id} item={item} />
           ))}
         </ul>
 
-        <div className="max-w-full mt-auto -mb-10 hidden md-lg:block">
+        <div className="max-w-full mt-auto -mb-10">
           <img
             src={Leaves}
             alt="leaves"
