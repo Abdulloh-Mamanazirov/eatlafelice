8ef6535690b5f5bf9553357d9a161129cae055f7
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-08 16:13:39 +0500

    add new images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,27 @@
 import Logo from "./logo.png";
+import LogoIcon from "./logo-icon.png";
 import Leaves from "./leaves.png";
 
+import About1 from "./about-image.jpg";
+import About2 from "./about-image-2.jpg";
+import About3 from "./about-image-3.jpg";
+
+import PastaIcon from "./pasta-icon.png";
+import PizzaIcon from "./pizza-icon.png";
+import PizzaSliceIcon from "./pizza-slice-icon.png";
+
+import MenuPageBg from "./menu-page-bg.jpg";
+
 export * from "./pizza";
-export { Logo, Leaves };
+export {
+  Logo,
+  Leaves,
+  About1,
+  About2,
+  About3,
+  LogoIcon,
+  PastaIcon,
+  PizzaIcon,
+  MenuPageBg,
+  PizzaSliceIcon,
+};
