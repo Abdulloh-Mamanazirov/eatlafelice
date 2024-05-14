d8e11ecfc7820eae64319f09b4e9b3a9609280a1
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-14 16:41:16 +0500

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
 import { Admin, Client } from "./layout";
 
 function App() {
-  return <Client />;
+  const path = window.location.pathname;
+  const [routes, setRoutes] = useState<React.JSX.Element>();
+
+  useEffect(() => {
+    if (path.startsWith("/admin")) {
+      setRoutes(<Admin />);
+    } else {
+      setRoutes(<Client />);
+      sessionStorage.removeItem("laFeliceAuthenticationToken");
+    }
+  }, [path]);
+
+  return routes;
 }
 
 export default App;
