127967579cf53d5b45a75e2b4baeaae35ae75d95
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-14 16:40:13 +0500

    change UI

--- src/pages/Admin/Login/index.tsx
+++ src/pages/Admin/Login/index.tsx
@@ -1,45 +1,77 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LogoIcon } from "../../../assets";
+
 const index = () => {
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("laFeliceAuthenticationToken");
+
+  useEffect(() => {
+    if (token) {
+      navigate("/admin/home");
+    }
+  }, [window.location.pathname]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { username, password } = e.target;
+    const data = {
+      username: username.value,
+      password: password.value,
+    };
+
+    if (data) {
+      navigate("/admin/home");
+      sessionStorage.setItem(
+        "laFeliceAuthenticationToken",
+        JSON.stringify(data)
+      );
+    }
+  };
+
   return (
-    <div className="bg-white rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-out">
-      <h2 className="p-3 text-3xl font-bold text-pink-400">Horiz</h2>
-      <div className="inline-block border-[1px] justify-center w-20 border-blue-400 border-solid"></div>
-      <h3 className="text-xl font-semibold text-blue-400 pt-2">Sign In!</h3>
-      {/* <div className="flex space-x-2 m-4 items-center justify-center">
-        <div className="socialIcon">
-          <Facebook />
+    <section className="flex justify-center relative">
+      <img
+        src="https://pagedone.io/asset/uploads/1702362010.png"
+        alt="gradient background image"
+        className="w-full h-screen object-cover fixed"
+      />
+      <div className="mx-auto max-w-lg px-6 lg:px-8 absolute pt-10">
+        <div className="bg-white rounded-2xl lg:mb-8 mb-5 py-3">
+          <img src={LogoIcon} alt="la felice logo" className="mx-auto" />
         </div>
-        <div className="socialIcon">
-          <GitHub />
+        <div className="rounded-2xl bg-white shadow-xl">
+          <form onSubmit={handleLogin} className="lg:p-11 p-7 mx-auto">
+            <div className="mb-11">
+              <h1 className="text-gray-900 text-center font-manrope text-3xl font-bold leading-10">
+                Welcome Back
+              </h1>
+            </div>
+            <input
+              name="username"
+              type="text"
+              required
+              className="w-full h-12 text-gray-900 placeholder:text-gray-400 text-lg font-normal leading-7 rounded-full border-gray-300 border shadow-sm focus:outline-none px-4 mb-6"
+              placeholder="Username"
+            />
+            <input
+              name="password"
+              type="password"
+              required
+              className="w-full h-12 text-gray-900 placeholder:text-gray-400 text-lg font-normal leading-7 rounded-full border-gray-300 border shadow-sm focus:outline-none px-4 mb-1"
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="w-full h-12 text-white text-center text-base font-semibold leading-6 rounded-full hover:bg-indigo-800 transition-all duration-700 bg-indigo-600 shadow-sm mt-6 mb-11"
+            >
+              Login
+            </button>
+          </form>
         </div>
-        <div className="socialIcon">
-          <Google />
-        </div>
-      </div> */}
-      {/* Inputs */}
-      <div className="flex flex-col items-center justify-center">
-        <input
-          type="email"
-          className="rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0"
-          placeholder="Email"
-        ></input>
-        <input
-          type="password"
-          className="rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0"
-          placeholder="Password"
-        ></input>
-        <button className="rounded-2xl m-2 text-white bg-blue-400 w-2/5 px-4 py-2 shadow-md hover:text-blue-400 hover:bg-white transition duration-200 ease-in">
-          Sign In
-        </button>
       </div>
-      <div className="inline-block border-[1px] justify-center w-20 border-blue-400 border-solid"></div>
-      <p className="text-blue-400 mt-4 text-sm">Don't have an account?</p>
-      <p
-        className="text-blue-400 mb-4 text-sm font-medium cursor-pointer"
-        onClick={() => "setIsLogin(false)"}
-      >
-        Create a New Account?
-      </p>
-    </div>
+    </section>
   );
 };
 export default index;
