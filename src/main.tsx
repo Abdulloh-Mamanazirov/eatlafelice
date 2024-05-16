7cb76f33b3c9b8f62658bd41aa4e238e62b34658
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-16 12:39:32 +0500

    set up axios & toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
+import axios from "axios";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import App from "./App";
 
+// CSS
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+
+// axios
+axios.defaults.baseURL = "http://37.77.104.188:8089/api";
+axios.defaults.headers.common["Authorization"] = sessionStorage.getItem(
+  "laFeliceAuthenticationToken"
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
+    <ToastContainer />
   </BrowserRouter>
 );
