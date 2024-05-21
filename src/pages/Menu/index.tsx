c39c6fb993f58a0e175b8206ee6e39cf534c17f5
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-05-21 20:47:32 +0500

    update mock data

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,38 +1,38 @@
-import type { IMenuItem } from "../../interfaces";
+import type { IMenuItem, IMenuItemItself } from "../../interfaces";
 import React from "react";
 import { MenuPageBg } from "../../assets";
 
 const mockData = [
   {
     id: 1,
-    image:
+    url:
       "https://thumbs.dreamstime.com/b/pepperoni-pizza-thinly-sliced-popular-topping-american-style-pizzerias-30402134.jpg",
-    title: "Sausage Pizza",
+    name: "Sausage Pizza",
     description: "One of the most delicious pizzas ever cooked!",
     price: 10,
   },
   {
     id: 2,
-    image:
+    url:
       "https://t3.ftcdn.net/jpg/01/49/45/34/360_F_149453469_SzBktO0iOxgNkIZNgWD33nBf9BTufRdy.jpg",
-    title: "Cheese Pizza",
+    name: "Cheese Pizza",
     description: "Wanna try cheese pizza? Come and taste it!",
     price: 8.5,
   },
   {
     id: 3,
-    image:
+    url:
       "https://www.allrecipes.com/thmb/Yf8b1aeXsPlq4YR7doCr6JOQndY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/6556969-390e7cdca370466fa397595aa76ee63e.jpg",
-    title: "Bucatini Pasta",
+    name: "Bucatini Pasta",
     description:
       "These long, hollow spaghetti-like tubes (aka perciatelli) are unusual and fun! Try them in casseroles or Asian stir-fries, or tossed with a fresh tomato sauce.",
     price: 5.5,
   },
   {
     id: 4,
-    image:
+    url:
       "https://www.allrecipes.com/thmb/MIUEgFQqjqSd1iCBiekdsyEIqMQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/122627_Bow-Ties-with-Sausage-Tomatoes-and-Cream_Photo-by-Elijah-3b861a830595401da6c617cfd91ac22b.jpg",
-    title: "Bow Tie Pasta (Farfalle)",
+    name: "Bow Tie Pasta (Farfalle)",
     description:
       "Use bow tie pasta to dress up any dish that calls for small pasta shapes, such as penne or shells. Also known as farfalle.",
     price: 7,
@@ -47,14 +47,14 @@ const MenuCard1 = ({ item }: IMenuItem): React.JSX.Element => {
           <img
             className="size-40 lg:size-36 aspect-square object-cover z-40 border-4 border-white rounded-full group-hover:border-8 group-hover:transition-all group-hover:duration-200 transition-all duration-200"
             id="avatar"
-            src={item.image}
+            src={item.url}
           />
           <div className="absolute bg-gray-100 z-10 size-[60%] w-full group-hover:size-[1%] group-hover:transition-all group-hover:duration-200 transition-all duration-200 delay-300 group-hover:delay-0" />
         </div>
       </div>
       <div className="headings *:text-center">
         <p className="text-xl font-serif font-semibold text-[#434955]">
-          {item.title}
+          {item.name}
         </p>
         <p className="w-fit mx-auto px-5 bg-secondary rounded-md text-white">
           ${Number(item.price).toLocaleString("us-Us")}
@@ -71,10 +71,10 @@ const MenuCard2 = ({ item }: IMenuItem): React.JSX.Element => {
     <div className="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:absolute before:top-0 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden shadow-md shadow-black/40">
       <img
         className="size-40 aspect-square object-cover bg-blue-700 mt-8 rounded-full border-2 border-white z-10 group-hover:scale-150 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500"
-        src={item.image}
+        src={item.url}
       />
       <div className="z-10 group-hover:-translate-y-10 transition-all duration-500">
-        <span className="text-2xl font-semibold">{item.title}</span>
+        <span className="text-2xl font-semibold">{item.name}</span>
         <p className="min-h-12 line-clamp-2 px-2">{item.description}</p>
       </div>
       <button className="bg-blue-700 px-4 py-1 mb-1 text-slate-50 rounded-md z-10 hover:scale-105 transition-all duration-500 hover:bg-blue-500">
@@ -132,7 +132,7 @@ const index = (): React.JSX.Element => {
         </h3>
       </div>
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-3 p-3">
-        {mockData.map((item) => (
+        {mockData.map((item:IMenuItemItself) => (
           <MenuCard1 key={item.id} item={item} />
         ))}
       </div>
