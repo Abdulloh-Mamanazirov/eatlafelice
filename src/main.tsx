8bd9f78096fe48e66e6c05a6ab0fa39f8629a99d
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-06-07 13:58:22 +0500

    add token to headers

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,9 @@ import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
 // axios
+const token = sessionStorage.getItem("laFeliceAuthenticationToken");
 axios.defaults.baseURL = "http://37.77.104.188:8089/api";
-axios.defaults.headers.common["Authorization"] =
-  "Bearer " + sessionStorage.getItem("laFeliceAuthenticationToken");
+if (token) axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
