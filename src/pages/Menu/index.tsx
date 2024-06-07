7a6f10e4b877676e7b9f7eb675c5ea6b9a32e3bd
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-06-07 13:58:49 +0500

    edit & delete items

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,53 +1,27 @@
+import axios from "axios";
 import type { IMenuItem, IMenuItemItself } from "../../interfaces";
-import React from "react";
+import { useState, useEffect } from "react";
 import { MenuPageBg } from "../../assets";
+import { BASE_URL } from "../../constants";
 
-const mockData = [
-  {
-    id: 1,
-    url:
-      "https://thumbs.dreamstime.com/b/pepperoni-pizza-thinly-sliced-popular-topping-american-style-pizzerias-30402134.jpg",
-    name: "Sausage Pizza",
-    description: "One of the most delicious pizzas ever cooked!",
-    price: 10,
-  },
-  {
-    id: 2,
-    url:
-      "https://t3.ftcdn.net/jpg/01/49/45/34/360_F_149453469_SzBktO0iOxgNkIZNgWD33nBf9BTufRdy.jpg",
-    name: "Cheese Pizza",
-    description: "Wanna try cheese pizza? Come and taste it!",
-    price: 8.5,
-  },
-  {
-    id: 3,
-    url:
-      "https://www.allrecipes.com/thmb/Yf8b1aeXsPlq4YR7doCr6JOQndY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/6556969-390e7cdca370466fa397595aa76ee63e.jpg",
-    name: "Bucatini Pasta",
-    description:
-      "These long, hollow spaghetti-like tubes (aka perciatelli) are unusual and fun! Try them in casseroles or Asian stir-fries, or tossed with a fresh tomato sauce.",
-    price: 5.5,
-  },
-  {
-    id: 4,
-    url:
-      "https://www.allrecipes.com/thmb/MIUEgFQqjqSd1iCBiekdsyEIqMQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/122627_Bow-Ties-with-Sausage-Tomatoes-and-Cream_Photo-by-Elijah-3b861a830595401da6c617cfd91ac22b.jpg",
-    name: "Bow Tie Pasta (Farfalle)",
-    description:
-      "Use bow tie pasta to dress up any dish that calls for small pasta shapes, such as penne or shells. Also known as farfalle.",
-    price: 7,
-  },
-];
+const Empty = () => {
+  return (
+    <div className="w-full text-center">
+      <span className="fa-solid fa-inbox text-4xl md:text-6xl" />
+      <p className="text-3xl tracking-wider">Empty</p>
+    </div>
+  );
+};
 
 const MenuCard1 = ({ item }: IMenuItem): React.JSX.Element => {
   return (
-    <div className="profile-card rounded-xl shadow-md shadow-black/50 overflow-hidden z-[100] relative cursor-pointer snap-start shrink-0 bg-white flex flex-col items-center justify-center gap-3 transition-all duration-200 border border-gray-200 group">
+    <div className="profile-card rounded-xl shadow-md shadow-black/50 overflow-hidden relative cursor-pointer snap-start shrink-0 bg-white flex flex-col items-center justify-center gap-3 transition-all duration-200 border border-gray-200 group">
       <div className="avatar w-full pt-5 flex items-center justify-center flex-col gap-1">
         <div className="img_container w-full flex items-center justify-center relative z-40 after:absolute after:h-[6px] after:w-full after:bg-primary after:top-4 after:group-hover:size-[1%] after:delay-200 after:group-hover:delay-0 after:group-hover:transition-all after:group-hover:duration-200 after:transition-all after:duration-200 before:absolute before:h-[6px] before:w-full before:bg-secondary before:bottom-4 before:group-hover:size-[1%] before:delay-200 before:group-hover:delay-0 before:group-hover:transition-all before:group-hover:duration-200 before:transition-all before:duration-200">
           <img
             className="size-40 lg:size-36 aspect-square object-cover z-40 border-4 border-white rounded-full group-hover:border-8 group-hover:transition-all group-hover:duration-200 transition-all duration-200"
             id="avatar"
-            src={item.url}
+            src={BASE_URL + "/api/images/" + item.url}
           />
           <div className="absolute bg-gray-100 z-10 size-[60%] w-full group-hover:size-[1%] group-hover:transition-all group-hover:duration-200 transition-all duration-200 delay-300 group-hover:delay-0" />
         </div>
@@ -66,25 +40,17 @@ const MenuCard1 = ({ item }: IMenuItem): React.JSX.Element => {
   );
 };
 
-const MenuCard2 = ({ item }: IMenuItem): React.JSX.Element => {
-  return (
-    <div className="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:absolute before:top-0 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden shadow-md shadow-black/40">
-      <img
-        className="size-40 aspect-square object-cover bg-blue-700 mt-8 rounded-full border-2 border-white z-10 group-hover:scale-150 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500"
-        src={item.url}
-      />
-      <div className="z-10 group-hover:-translate-y-10 transition-all duration-500">
-        <span className="text-2xl font-semibold">{item.name}</span>
-        <p className="min-h-12 line-clamp-2 px-2">{item.description}</p>
-      </div>
-      <button className="bg-blue-700 px-4 py-1 mb-1 text-slate-50 rounded-md z-10 hover:scale-105 transition-all duration-500 hover:bg-blue-500">
-        ${Number(item.price).toLocaleString("us-Us")}
-      </button>
-    </div>
-  );
-};
-
 const index = (): React.JSX.Element => {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    await axios.get("/menus").then((res) => setData(res.data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       {/* header */}
@@ -131,17 +97,15 @@ const index = (): React.JSX.Element => {
           Meet our Menu <span className="fa-solid fa-utensils" />
         </h3>
       </div>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-3 p-3">
-        {mockData.map((item:IMenuItemItself) => (
-          <MenuCard1 key={item.id} item={item} />
-        ))}
-      </div>
-      <br />
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-3 p-3">
-        {mockData.map((item) => (
-          <MenuCard2 key={item.id} item={item} />
-        ))}
-      </div>
+      {data?.length > 0 ? (
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-3 p-3">
+          {data?.map((item: IMenuItemItself) => (
+            <MenuCard1 key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <Empty />
+      )}
     </>
   );
 };
